fc5deb75ab75c8e2f1e6c436224113a6273b1321
Kuzless <deadgoatbro@gmail.com>
2025-05-12 13:28:19 +0300

    Added dropdowns and js script for edit adv page

---
+++ AdvBoard.MVC/wwwroot/js/cascading-dropdown.js
@@ -0,0 +1,22 @@
+﻿function popSubCategories() {
+    var categoryId = document.getElementById("CategoryId").value;
+    var subCategorySelect = document.getElementById("SubCategoryId");
+
+    subCategorySelect.innerHTML = "";
+    var subCategories = JSON.parse(document.getElementById("SubCategoryData").value);
+
+    var filteredSubCategories = subCategories.filter(function (subCategory) {
+        return subCategory.CategoryId == categoryId;
+    });
+
+    filteredSubCategories.forEach(function (subCategory) {
+        var option = document.createElement("option");
+        option.value = subCategory.Id;
+        option.textContent = subCategory.Name;
+        subCategorySelect.appendChild(option);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    popSubCategories();
+});
